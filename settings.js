a2e6c47dbe2e02f4ff5393e48bd4ebc475f9a7d8
M.Husnain <161164286+husnain773786@users.noreply.github.com>
2024-04-05 13:59:17 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,11 +23,11 @@ global.APIs = {
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = 'ᴠᴀᴊɪʀᴀ ʀᴀᴛʜɴᴀʏᴀᴋᴀ';
-global.owner = ['94719199757','94703475691'];
+global.owner = ['971557183506'];
 global.premium = ['94719199757','94703475691'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
-global.caption = ' 👨‍💻 ＶＡＪＩＲＡ ＭＤ V3 👨‍💻';
+global.author = 'ᴹᴿ°᭄HUSNAIN❖࿐';
+global.caption = ' *👨‍💻 ᴹᴿ°᭄HUSNAIN❖࿐ 👨‍💻*';
 global.sessionName = 'session';
 global.prefa = ['!', '.'];
 global.sp = '✪';
